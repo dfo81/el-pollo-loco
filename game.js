4eb270038bca766cecd0d7e4031beea05a85256e
Dieter Foos <d.foos@mail.de>
2026-01-11 19:41:17 +0100

    buttons added

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ let sounds = new Sounds();
 let isKeyDown = {};
 let gameTasks = [];
 let gamePaused = false;
+let music = localStorage.getItem("music") === "true";
 
 window.addEventListener("keydown", (e) => {
   if (!isKeyDown[e.code]) {
@@ -18,39 +19,60 @@ window.addEventListener("keydown", (e) => {
 
 window.addEventListener("keyup", (e) => {
   isKeyDown[e.code] = false;
-  if (e.code === "ArrowLeft") (keyboard.LEFT = false);
-  if (e.code === "ArrowRight") (keyboard.RIGHT = false);
+  if (e.code === "ArrowLeft") keyboard.LEFT = false;
+  if (e.code === "ArrowRight") keyboard.RIGHT = false;
 });
 
+function init() {
+  if (music) {
+    document.getElementById("music").classList.remove("music-off");
+    document.getElementById("music").classList.add("music-on");
+    sounds.MUSIC.play();
+  } else {
+    document.getElementById("music").classList.add("music-off");
+    document.getElementById("music").classList.remove("music-on");
+  }
+}
+
 function startGame() {
-  document.getElementById('startScreen').classList.add('d-none')
-  sounds.MUSIC.play();
+  document.getElementById("startScreen").classList.add("d-none");
   canvas = document.getElementById("canvas");
   world = new World(canvas, keyboard, sounds);
   world.startGame();
-document.getElementsByClassName('menu')[0].classList.add('d-none'); 
+  document.getElementsByClassName("menu")[0].classList.add("d-none");
 }
 
 setInterval(() => {
-    if (gamePaused) return;
-    gameTasks.forEach(task => {
-        let now = Date.now();
-        if (now - task.lastRun >= task.interval) {
-            task.action();
-            task.lastRun = now;
-        }
-    });
+  if (gamePaused) return;
+  gameTasks.forEach((task) => {
+    let now = Date.now();
+    if (now - task.lastRun >= task.interval) {
+      task.action();
+      task.lastRun = now;
+    }
+  });
 }, 1000 / 120);
 
 function addGameTask(owner, action, interval) {
-    gameTasks.push({ 
-        owner: owner, 
-        action: action, 
-        interval: interval, 
-        lastRun: 0 
-    });
+  gameTasks.push({
+    owner: owner,
+    action: action,
+    interval: interval,
+    lastRun: 0,
+  });
 }
 
 function removeTasksOfObject(owner) {
-    gameTasks = gameTasks.filter(task => task.owner !== owner);
+  gameTasks = gameTasks.filter((task) => task.owner !== owner);
+}
+
+function toggleMusic() {
+  music = !music;
+  if (music) {
+    sounds.MUSIC.play();
+  } else {
+    sounds.MUSIC.stop();
+  }
+  localStorage.setItem("music", music);
+  init();
 }
